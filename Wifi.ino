8bd5d3b0c93537c92cd318247acd9cf74697690f
marwijn <marwijn.christiaans@gmail.com>
2016-05-27 19:12:07 +0200

    Start in AP mode when unable to connect to AP

--- Wifi.ino
+++ Wifi.ino
@@ -1,22 +1,33 @@
-const char* ssid = "Sitecom0E50B3";
-const char* password = "4NDSF3V4B4W9";
+const char* ssid = "Marwijn";
+const char* password = "MaMoCoQu";
+
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 1, 1);
+DNSServer dnsServer;
 
 void initWifi()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
+
+
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP("DNSServer CaptivePortal example");
+
+    dnsServer.start(DNS_PORT, "*", apIP);
   }
-  if (!MDNS.begin("esp8266")) {
+
+  if (!MDNS.begin("esp8266"))
+  {
     Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
   }
-  MDNS.addService("http", "tcp", 80);
-  Serial.println("mDNS responder started");
+  else
+  {
+    MDNS.addService("http", "tcp", 80);
+    Serial.println("mDNS responder started");
+  }
 }
 
